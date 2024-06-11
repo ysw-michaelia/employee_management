d8c20f90fe3444fd2447c2aa14739f0eee02131e
shuwen yang <yangshuwen7560@gmail.com>
2024-06-11 19:47:47 +0200

    changed get method for lookup info

--- server.js
+++ server.js
@@ -72,9 +72,47 @@ app.post('/api/projectassignments', async (req, res) => {
     }
 });
 
-app.get('/api/projectassignments', async (req, res) => {
+app.get('/api/latestassignments', async (req, res) => {
     try {
-        const projectAssignments = await ProjectAssignment.find();
+        const projectAssignments = await ProjectAssignment.aggregate([
+            {
+                $lookup: {
+                    from: 'employees',
+                    localField: 'employee_id',
+                    foreignField: 'employee_id',
+                    as: 'employee'
+                }
+            },
+            {
+                $unwind: '$employee'
+            },
+            {
+                $lookup: {
+                    from: 'projects', 
+                    localField: 'project_code',
+                    foreignField: 'project_code',
+                    as: 'project'
+                }
+            },
+            {
+                $unwind: '$project'
+            },
+            {
+                $project: {
+                    _id: 0,
+                    employee_id: 1,
+                    full_name: '$employee.full_name',
+                    project_name: '$project.project_name',
+                    start_date: 1
+                }
+            },
+            {
+                $sort: { start_date: -1 }
+            },
+            {
+                $limit: 5 // Limit the results to 5
+            }
+        ]);
         res.json(projectAssignments);
     } catch (error) {
         console.error('Error fetching project assignments:', error);
